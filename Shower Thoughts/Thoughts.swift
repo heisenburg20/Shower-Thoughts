5511455cf3c786930e7e26cda8085456ef8e1007
Tawanda Chandiwana <tawandachandiwana@Tawandas-iMac.local>
2021-08-10 12:16:37 +0200

    Displays content on UITableView

---
+++ Shower Thoughts/Thoughts.swift
@@ -0,0 +1,26 @@
+//
+//  Thoughts.swift
+//  Shower Thoughts
+//
+//  Created by Tawanda Chandiwana on 2021/08/09.
+//
+
+import Foundation
+
+struct ShowerThoughts: Identifiable{
+    var id : Int
+    var title: String
+}
+var showerThoughts =  [
+            ShowerThoughts(id: 0, title: "paper clips are just staples with less commitment"),
+            ShowerThoughts(id: 1, title: "earlier it was like what does not kill you makes you stronger, nowadays it is what does not kill you mutates, and then tries to kill you over and over again"),
+            ShowerThoughts(id: 3, title: "Tinder and Spotify should pair up and match with people with the same music tastes"),
+            ShowerThoughts(id: 4, title: "adult life is hard but at least we dont hate to do P.E anymore"),
+            ShowerThoughts(id: 5, title: "'F*ck off' backward is still 'F*ck off' in Irish accent"),
+            ShowerThoughts(id: 6, title: "the more you admit when you are wring, the more listen to you when you are right"),
+            ShowerThoughts(id: 7, title: "a country's flag is technically thier logo, and the anthme is their theme song"),
+            ShowerThoughts(id: 8, title: "biting your toungue is agood example of how things can go wrong evene with years of experience"),
+            ShowerThoughts(id: 9, title: "it's 2021 and car designers still havent realized that everyone needs a garbage can in their cars"),
+            ShowerThoughts(id: 10, title: "hearing 'you are on mute' is embarassing but not as much as hearing 'you are not on mute'")
+        ]
+ 
